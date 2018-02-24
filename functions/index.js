0c9129ea53f89025176c4aa30c7596bd6926594d
therese <tdiede@gmail.com>
2018-02-24 14:17:50 -0800

    add request library; webhook function for slack

--- functions/index.js
+++ functions/index.js
@@ -8,3 +8,24 @@ const request = require('request');
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+
+
+const WEBHOOK_URL = 'https://hooks.slack.com/services/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';
+
+// Reads the content of the node that triggered the function and sends it to the registered Webhook
+// URL.
+exports.webhook = functions.database.ref('/hooks/{hookId}').onWrite((event) => {
+  return request({
+    uri: WEBHOOK_URL,
+    method: 'POST',
+    json: true,
+    body: event.data.val(),
+    resolveWithFullResponse: true,
+  }).then((response) => {
+    if (response.statusCode >= 400) {
+      throw new Error(`HTTP Error: ${response.statusCode}`);
+    }
+    return console.log('SUCCESS! Posted', event.data.ref);
+  });
+});

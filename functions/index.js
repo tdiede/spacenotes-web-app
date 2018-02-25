67421f9996367763fe6c0887fc956226b16cb314
therese <tdiede@gmail.com>
2018-02-24 16:09:11 -0800

    install peer dependency; try request-promise

--- functions/index.js
+++ functions/index.js
@@ -1,31 +1,40 @@
 const functions = require('firebase-functions');
-const request = require('request');
+const request = require('request-promise');
+
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+// spacenotes
+exports.spacenotes = functions.https.onRequest((request, response) => {
+ response.send(
+ 	`Hello from Therese! Insert web interface here.
+ 	Display all our database on the web.`
+ );
 });
 
 
+const WEBHOOK_URL = 'https://hooks.slack.com/services/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';
 
-const WEBHOOK_URL = 'https://hooks.slack.com/services/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';
-
-// Reads the content of the node that triggered the function and sends it to the registered Webhook
-// URL.
-exports.webhook = functions.database.ref('/hooks/{hookId}').onWrite((event) => {
+exports.writeWebhook = functions.database.ref('/maps/{mapId}').onWrite((event) => {
   return request({
     uri: WEBHOOK_URL,
     method: 'POST',
     json: true,
-    body: event.data.val(),
+    body: '{"text":"Hello, World!"}',
     resolveWithFullResponse: true,
   }).then((response) => {
     if (response.statusCode >= 400) {
-      throw new Error(`HTTP Error: ${response.statusCode}`);
+      throw new Error(`HTTP Error, customized: ${response.statusCode}`);
     }
     return console.log('SUCCESS! Posted', event.data.ref);
   });
 });
+
+
+// return request.post(WEBHOOK_URL, { json: { text: `New map from Firebase!` }});
